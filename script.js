9df6a6cc5e4bf8f1ff0a4c05bb94ec32fc4ad884
mfaskdr10 <mfaskdr10@gmail.com>
2025-01-25 16:45:03 +0700

    Refactor HTML structure and enhance JavaScript functionality for better navigation control; add About section with styling adjustments

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ const hamburger = document.getElementById("hamburger");
 hamburger.addEventListener("click", () => {
   navMenu.classList.toggle("active");
 });
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navMenu.contains(e.target)) {
+    navMenu.classList.remove("active");
+  }
+});
