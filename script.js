c7e3a932e29a12575b001ba5766f748919b1963e
M. Fadhil Alvan Syah <80678865+mfaskdr10@users.noreply.github.com>
2025-01-26 07:56:40 +0700

    Add new blog page and enhance navigation with scroll effects

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const navMenu = document.getElementById("nav-menu");
 const hamburger = document.getElementById("hamburger");
 
-hamburger.addEventListener("click", () => {
+const hamburgerClick = hamburger.addEventListener("click", () => {
   navMenu.classList.toggle("active");
 });
 
@@ -9,4 +9,19 @@ document.addEventListener("click", function (e) {
   if (!hamburger.contains(e.target) && !navMenu.contains(e.target)) {
     navMenu.classList.remove("active");
   }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > 50) {
+      document.getElementById("navbar").classList.add("fixed");
+      document.getElementById("hero-container").style.marginTop = "120px";
+      document.getElementById("hero-container").style.transition = "all 1s";
+      navMenu.style.top = "6.5rem";
+    } else {
+      navMenu.style.top = "5.5rem";
+      document.getElementById("navbar").classList.remove("fixed");
+      document.getElementById("hero-container").style.marginTop = "0";
+    }
+  });
 });
